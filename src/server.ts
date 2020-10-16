ac580c62754586c4a3a94a8fe3a98265030bdfa2
Jonathan <jonathanthomaz96@gmail.com>
2020-10-16 17:14:29 -0300

    create server express

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+app.get('/users', (request, response) =>{ return response.send({"message":"Hello World"})});
+
+app.listen(3333);
