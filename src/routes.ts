43061a09a11567b7d6eb2fdc5c1753f53764ef60
Jonathan <jonathanthomaz96@gmail.com>
2020-10-16 21:56:33 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ import OrphanagesController from './controllers/OrphanagesController';
 const routes = Router();
 
 routes.get('/orphanages', OrphanagesController.getAll);
-routes.get('/orphanage/:id', OrphanagesController.get);
+routes.get('/orphanages/:id', OrphanagesController.get);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
