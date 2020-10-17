50a8f90357abdd4c21db737b0ec3489902558740
Jonathan <jonathanthomaz96@gmail.com>
2020-10-16 21:45:53 -0300

    create getAll and route

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 
+routes.get('/orphanages', OrphanagesController.getAll);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
