273f2b2114f83d1319f72078b8f2c9f62dff1407
Jonathan <jonathanthomaz96@gmail.com>
2020-10-17 00:43:17 -0300

    add .env variables

--- src/server.ts
+++ src/server.ts
@@ -14,4 +14,4 @@ app.use('/uploads', express.static(path.join(__dirname, '..','uploads')));
 app.use(errorHandler);
 
 
-app.listen(3333);
+app.listen(process.env.APP_PORT);
--- src/views/images_view.ts
+++ src/views/images_view.ts
@@ -6,7 +6,7 @@ export default {
     render(image: Image) {
         return {
             id: image.id,
-            url: `http://localhost:3333/uploads/${image.path}`
+            url: `${process.env.BASE_URL_IMG}:${process.env.APP_PORT}/uploads/${image.path}`
         };
     },
 
